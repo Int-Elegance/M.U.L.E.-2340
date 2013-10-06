bd04cde79be85a81541a5702b31a07cebcf3b02a
lwinston7 <lwinston@gatech.edu>
2013-10-06 16:03:36 -0400

    Game and Game Panel

---
+++ MainProject/src/Game.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+
+
+public class Game {
+
+	List<String> rounds; //TODO: change type to Round
+	GamePanel map;
+	List<Actor> players;
+	
+	public boolean login()
+	{
+		//TODO: implement
+		return false;
+	}
+	
+	public boolean initializer()
+	{
+		//TODO: implement
+		return false;
+	}
+	
+	public boolean play()
+	{
+		//TODO: implement
+		return false;
+	}
+}
