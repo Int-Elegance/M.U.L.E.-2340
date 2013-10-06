916de229e803275b7351f0060b33aebfc6a4f375
mjmaurer <mjmaurer7@gatech.edu>
2013-10-06 15:57:32 -0400

    Getters and Setters and private instance data

--- MainProject/src/Actor.java
+++ MainProject/src/Actor.java
@@ -10,5 +10,23 @@ public abstract class Actor {
 		this.image = image;
 		this.location = location;
 	}
+
+	public String getImage() {
+		return image;
+	}
+
+	//TODO: Change type of image to "ImageIcon" and type of location to "Tile"
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+	
+	//TODO: Change type of image to "ImageIcon" and type of location to "Tile"
+	public void setLocation(String location) {
+		this.location = location;
+	}
 	
 }
--- MainProject/src/Player.java
+++ MainProject/src/Player.java
@@ -2,15 +2,15 @@ import java.util.List;
 
 public class Player extends Actor {
 	
-	int race;
-	String name;
-	String color;
-	int score = 0;
-	int money = 0;
-	int food = 0;
-	int energy = 0;
-	int ore = 0;
-	List<String> mules; //TODO: change type to MULE
+	private int race;
+	private String name;
+	private String color;
+	private int score = 0;
+	private int money = 0;
+	private int food = 0;
+	private int energy = 0;
+	private int ore = 0;
+	private List<String> mules; //TODO: change type to MULE
 	
 	public Player(String image, String tile, int race, String name, String color)
 	{
@@ -20,6 +20,80 @@ public class Player extends Actor {
 		this.color = color;
 		
 		//TODO: calculate the player's money
+	}
+
+	public int getRace() {
+		return race;
+	}
+
+	public void setRace(int race) {
+		this.race = race;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+
+	public int getFood() {
+		return food;
+	}
+
+	public void setFood(int food) {
+		this.food = food;
+	}
+
+	public int getEnergy() {
+		return energy;
+	}
+
+	public void setEnergy(int energy) {
+		this.energy = energy;
+	}
+
+	public int getOre() {
+		return ore;
+	}
+
+	public void setOre(int ore) {
+		this.ore = ore;
+	}
+
+	//TODO make sure it works with Mule class
+	public List<String> getMules() {
+		return mules;
+	}
+	
+	//TODO make sure it works with Mule class
+	public void addMule(String mule) {
+		mules.add(mule);
 	}
 
 }
