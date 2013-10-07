cd424dd3fd7abdbcffdc14f7dfd7247c6a977be3
mjmaurer <mjmaurer7@gatech.edu>
2013-10-06 23:48:15 -0400

    Login Class Update
    Login Frame now uses CardLayout in order to transition between player
    login screens. Buttons work to transition. Information is now added to
    appropriate classes from login. Design is updated as well. Game class is
    updated to be able to add players. Still need to add comments.

--- MainProject/src/Game.java
+++ MainProject/src/Game.java
@@ -1,10 +1,17 @@
 import java.util.List;
+import java.util.ArrayList;
+
+
 
 public class Game {
 
 	List<String> rounds; //TODO: change type to Round
-	GamePanel map;
+	//GamePanel map;
 	List<Actor> players;
+	
+	public Game() {
+		players = new ArrayList<>();
+	}
 	
 	public boolean login()
 	{
@@ -22,5 +29,13 @@ public class Game {
 	{
 		//TODO: implement
 		return false;
+	}
+	
+	public void addPlayer(Player p){
+		players.add(p);
+	}
+	
+	public List<Actor> getPlayers() {
+		return players;
 	}
 }
--- MainProject/src/Login.java
+++ MainProject/src/Login.java
@@ -2,29 +2,32 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
-import java.awt.GridLayout;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-import com.jgoodies.forms.factories.FormFactory;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.CardLayout;
+import javax.swing.JTextField;
+import java.util.ArrayList;
 
 
 public class Login {
 	
+	//TODO add actual map and difficulty to different classes
 	private String difficulty;
 	private String mapType;
 	private int numPlayers;
 
 	private JFrame Login;
+	private JTextField nameField;
+	private ArrayList<String> colors = new ArrayList<>();
+	private ArrayList<String> races = new ArrayList<>();
+	
+	private Game game;
 
 	/**
 	 * Launch the application.
@@ -34,6 +37,8 @@ public class Login {
 			public void run() {
 				try {
 					Login window = new Login();
+					window.Login.pack();
+					window.Login.setSize(450, 250);
 					window.Login.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -53,76 +58,136 @@ public class Login {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
+		colors.add("Red");
+		colors.add("Green");
+		colors.add("Blue");
+		colors.add("Yellow");
+		colors.add("Orange");
+		colors.add("Purple");
+		
+		races.add("Human");
+		races.add("Flapper");
+		races.add("Bonzoid");
+		races.add("Ugaite");
+		races.add("Buzzite");
+		
 		Login = new JFrame();
 		Login.setTitle("Login\r\n");
+		final CardLayout cl = new CardLayout();
+		Login.getContentPane().setLayout(cl);
 		
-		JPanel panel = new JPanel();
-		Login.getContentPane().add(panel, BorderLayout.CENTER);
-		panel.setLayout(new FormLayout(new ColumnSpec[] {
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				ColumnSpec.decode("default:grow"),
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,},
-			new RowSpec[] {
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,}));
+		game = new Game();
 		
-		JLabel lblSelectDifficulty = new JLabel("Select Difficulty");
-		panel.add(lblSelectDifficulty, "10, 10");
+		JPanel login = new JPanel();
+		Login.getContentPane().add(login, "login");
+		login.setLayout(null);
 		
-		JComboBox difficultyBox = new JComboBox();
+		JLabel lblWelcomeToMule = new JLabel("Welcome to MULE!");
+		lblWelcomeToMule.setBounds(106, 25, 162, 14);
+		login.add(lblWelcomeToMule);
+		
+		JLabel lblSelectDifficulty = new JLabel("Difficulty");
+		lblSelectDifficulty.setBounds(106, 79, 89, 14);
+		login.add(lblSelectDifficulty);
+		
+		final JComboBox difficultyBox = new JComboBox();
+		difficultyBox.setBounds(226, 76, 83, 20);
 		difficultyBox.setModel(new DefaultComboBoxModel(new String[] {"Beginner", "Standard", "Tournament"}));
-		panel.add(difficultyBox, "14, 10, left, default");
+		login.add(difficultyBox);
 		
-		JLabel lblMapType = new JLabel("Map Type");
-		panel.add(lblMapType, "10, 12");
+		JLabel lblSelectMapType = new JLabel("Map Type");
+		lblSelectMapType.setBounds(106, 105, 89, 14);
+		login.add(lblSelectMapType);
 		
-		JComboBox mapTypeBox = new JComboBox();
-		mapTypeBox.setModel(new DefaultComboBoxModel(new String[] {"Default", "Random"}));
-		panel.add(mapTypeBox, "14, 12, left, default");
+		final JComboBox mapBox = new JComboBox();
+		mapBox.setBounds(226, 102, 100, 20);
+		mapBox.setModel(new DefaultComboBoxModel(new String[] {"Standard", "Random"}));
+		login.add(mapBox);
 		
 		JLabel lblNumberOfPlayers = new JLabel("Number of Players");
-		panel.add(lblNumberOfPlayers, "10, 14");
+		lblNumberOfPlayers.setBounds(106, 131, 110, 14);
+		login.add(lblNumberOfPlayers);
 		
-		JSpinner playerBox = new JSpinner();
-		playerBox.setModel(new SpinnerNumberModel(2, 2, 4, 1));
-		panel.add(playerBox, "14, 14, left, default");
+		final JSpinner numPlayerBox = new JSpinner();
+		numPlayerBox.setBounds(226, 128, 31, 20);
+		numPlayerBox.setModel(new SpinnerNumberModel(2, 2, 4, 1));
+		login.add(numPlayerBox);
 		
 		JButton btnEnter = new JButton("Enter");
 		btnEnter.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			
+			public void actionPerformed(ActionEvent e) {
+				//TODO add to respective cl
+				difficulty = (String)difficultyBox.getSelectedItem();
+				mapType = (String) mapBox.getSelectedItem();
+				numPlayers = (int) numPlayerBox.getValue();
+				for (int i = 0; i < numPlayers; i++) {
+					Player newPlayer = new Player(null, null, 0, null, null);
+					game.addPlayer(newPlayer);
+					generatePlayerPanel(newPlayer, i, cl);
+				}
+				cl.next(Login.getContentPane());
 			}
 		});
-		panel.add(btnEnter, "10, 16");
+		btnEnter.setBounds(106, 154, 89, 23);
+		login.add(btnEnter);
+		
+		JLabel lblPleaseEnterYour = new JLabel("Please enter your settings");
+		lblPleaseEnterYour.setBounds(106, 42, 151, 14);
+		login.add(lblPleaseEnterYour);
 	}
-
+	
+	private void generatePlayerPanel(final Player p, final int num, final CardLayout cl){
+		JPanel playerPanel = new JPanel();
+		playerPanel.setLayout(null);
+		Login.getContentPane().add(playerPanel, ""+ num);
+		
+		JLabel titleLabel = new JLabel("Player "+(num+1)+" Attributes");
+		titleLabel.setBounds(106, 31, 130, 14);
+		playerPanel.add(titleLabel);
+		
+		JLabel nameLabel = new JLabel("Name");
+		nameLabel.setBounds(106, 79, 89, 14);
+		playerPanel.add(nameLabel);
+		
+		JLabel raceLabel = new JLabel("Race");
+		raceLabel.setBounds(106, 105, 89, 14);
+		playerPanel.add(raceLabel);
+		
+		final JComboBox raceBox = new JComboBox();
+		raceBox.setModel(new DefaultComboBoxModel(races.toArray()));
+		raceBox.setBounds(226, 102, 69, 20);
+		playerPanel.add(raceBox);
+		
+		JLabel colorLabel = new JLabel("Color");
+		colorLabel.setBounds(106, 131, 89, 14);
+		playerPanel.add(colorLabel);
+		
+		final JTextField nameField = new JTextField();
+		nameField.setBounds(223, 76, 111, 20);
+		playerPanel.add(nameField);
+		nameField.setColumns(10);
+		
+		final JComboBox colorBox = new JComboBox();
+		colorBox.setModel(new DefaultComboBoxModel(colors.toArray()));
+		colorBox.setBounds(226, 128, 69, 20);
+		playerPanel.add(colorBox);
+		
+		JButton button = new JButton("Enter");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				p.setRace(races.indexOf((String)raceBox.getSelectedItem()));
+				p.setName(nameField.getText());
+				p.setColor((String) colorBox.getSelectedItem());
+				if (num == numPlayers - 1) {
+					//TODO show main game screen
+				} else {
+					cl.next(Login.getContentPane());
+				}
+			}
+		});
+		button.setBounds(106, 154, 89, 23);
+		playerPanel.add(button);
+		
+	}
 }
