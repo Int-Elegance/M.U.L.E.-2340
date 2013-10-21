f07c5d4f0e715ab4066c79d3bb381d62f5688bcc
lwinston7 <lwinston@gatech.edu>
2013-10-21 17:40:39 -0400

    Notification Panel changes
    Added display for rounds and turns in notification panel

--- MainProject/src/NotificationPanel.java
+++ MainProject/src/NotificationPanel.java
@@ -8,28 +8,36 @@ import java.util.List;
 public class NotificationPanel extends JPanel {
 
 	private Game game;
+	private JLabel turn;
 	private JLabel score;
 	private JLabel time;
 	private JButton pass;
+	private JLabel round;
 	
 	public NotificationPanel(Game g)
 	{
 		game = g;
 		score = new JLabel();
 		time = new JLabel();
+		turn = new JLabel();
+		round = new JLabel();
 		pass = new JButton("Pass");
 		pass.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (game.getCurrentTurn() instanceof LandSelectionTurn) ((LandSelectionTurn) game.getCurrentTurn()).pass();
 			}
 		});
-		this.add(score, BorderLayout.NORTH);
+		
+		this.add(round, BorderLayout.SOUTH);
+		this.add(turn, BorderLayout.NORTH);
+		this.add(score, BorderLayout.CENTER);
 		this.add(time, BorderLayout.WEST);
 		this.add(pass, BorderLayout.EAST);
 	}
 	
 	public void paintComponent(Graphics g)
 	{
+		Turn currentTurn = game.getCurrentTurn();
 		super.paintComponent(g);
 		List<Player> players = game.getPlayers();
 		String str = "";
@@ -39,12 +47,13 @@ public class NotificationPanel extends JPanel {
 			str += p.getName() + " " + p.getMoney() + " ";
 		}
 		
+		round.setText("Current Round: " + currentTurn.round.getRoundNumber());
+		turn.setText("Current Turn: " + currentTurn.player.getName());
 		score.setText("Score: " + str);
-		time.setText("Time Remaining: " + game.getCurrentTurn().getSecondsLeft());
+		time.setText("Time Remaining: " + currentTurn.getSecondsLeft());
 	}
+	
 	public void update(){
-		time.repaint();
-		score.repaint();
 		this.repaint();
 		this.validate();
 	}
--- MainProject/src/Round.java
+++ MainProject/src/Round.java
@@ -77,6 +77,9 @@ public class Round {
     	if(notificationPanel!=null){
     		notificationPanel.update();
     	}
-
     }
+    
+   public int getRoundNumber() {
+	   return roundNumber;
+   }
 }
